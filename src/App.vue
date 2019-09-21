ee7635abacaef557e61ebcc55d446f4c302d5ad7
Jonny Tyson <jonnytyson@live.com>
2019-09-20 19:19:37 -0700

    Initial Setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="max-width">
+    <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+  @import url("https://fonts.googleapis.com/css?family=Rubik");
+
+  #app {
+    font-family: 'Rubik', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    margin-bottom: 60px;
+    font-size: 20px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+
+  body {
+    background: #ffffff;
+    background-image: url("./assets/background.jpg");
+    margin: 0;
+  }
+
+  .max-width {
+    max-width: 1400px;
+    margin: 0 auto;
+  }
+
+   @media (min-width: 1000px) and (max-width: 1400px) {
+    .max-width {
+      max-width: 85vw;
+      margin: 0 auto;
+      font-size: 1.4vw;
+    }
+   }
+
+    @media (min-width: 1px) and (max-width: 999px) {
+    #app {
+      margin-top: 0px;
+      margin-bottom: 0px;
+      font-size: 9px;
+    }
+
+    .max-width {
+      max-width: 100%;
+      margin: 0 auto;
+      font-size: 13px;
+    }
+   }
 </style>
